d8e4a5d727bb4f3c237bde8b16a247d5ff8d4105
Abdullah Jbero <81176026+Abdullah023313@users.noreply.github.com>
2023-01-20 15:07:42 +0300

    Add-commit-to-aletr.ts

--- src/app/shared/alert/models/alert.ts
+++ src/app/shared/alert/models/alert.ts
@@ -1,5 +1,7 @@
 import { AlertType } from "./AlertType";
 
+//Turn off the alarm if there was a previous alarm,
+//then work for 3s, then turn off
 export class alert{
     public  type: string
     constructor(type:AlertType, public message: string ){
