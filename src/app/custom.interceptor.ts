a5e6d85553a7ac1c6ac84ebbe3e19cb658c30848
Abdullah <81176026+Abdullah023313@users.noreply.github.com>
2023-03-06 16:37:22 +0300

    install-firebase-admin

--- src/app/custom.interceptor.ts
+++ src/app/custom.interceptor.ts
@@ -11,10 +11,10 @@ import { UserService } from './services/user.service';
 @Injectable()
 export class CustomInterceptor implements HttpInterceptor {
 
-  constructor(private userService :UserService) {}
+  constructor(private userService: UserService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    let modifiedRequest=request.clone({setHeaders:{'authentication':`Bearer ${this.userService.getToken()} `}})
+    let modifiedRequest = request.clone({ setHeaders: { 'authentication': `Bearer ${this.userService.getToken()} ` } })
     return next.handle(modifiedRequest);
   }
 }
--- src/app/shared/select-language/select-language.component.ts
+++ src/app/shared/select-language/select-language.component.ts
@@ -7,19 +7,21 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./select-language.component.css']
 })
 export class SelectLanguageComponent {
-  constructor(private translateService: TranslateService) { }
+  constructor(private translateService: TranslateService) {
+    this.changeLang();
+  }
 
   LangArray = [{ name: "English", value: "en" },
   { name: "Arabic", value: "ar" }]
 
   defaultLanguage(): string {
     let storageLanguage = localStorage.getItem('Language')?.toString();
-    if (typeof storageLanguage !== 'undefined' ) {
+    if (typeof storageLanguage !== 'undefined') {
       return storageLanguage;
     }
     return 'English';
   }
-  currentLanguage:string=this.defaultLanguage();
+  currentLanguage: string = this.defaultLanguage();
   changeLang() {
     localStorage.setItem('Language', this.currentLanguage);
     this.translateService.use(this.LangArray.find(n => n.name == this.currentLanguage)?.value!);
